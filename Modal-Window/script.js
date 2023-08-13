38e83dcff8569de6729a508c3c003afb185fe5b3
Joseph Maddox <Lavertsss@gmail.com>
2023-08-13 17:29:27 -0400

    Update script.js

--- Modal-Window/script.js
+++ Modal-Window/script.js
@@ -1,1 +1,6 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModel = document.querySelectorAll(".open-modal");
+console.log(modal, overlay, btnCloseModal, btnsOpenModel);
