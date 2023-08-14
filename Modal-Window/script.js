c265d8d24ed157eeee52187166651ba6973c343f
Joseph Maddox <Lavertsss@gmail.com>
2023-08-13 20:05:00 -0400

    finished modal

--- Modal-Window/script.js
+++ Modal-Window/script.js
@@ -2,5 +2,23 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModel = document.querySelectorAll(".open-modal");
-console.log(modal, overlay, btnCloseModal, btnsOpenModel);
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+console.log(modal, overlay, btnCloseModal, btnsOpenModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  console.log("You've clicked a button.");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
