f224d8bbdedab40ba9d5fb2d59b53a86edebbea7
Joseph Maddox <Lavertsss@gmail.com>
2023-09-17 14:40:04 -0400

    update

--- First Proffessional/script.js
+++ First Proffessional/script.js
@@ -1,1 +1,7 @@
 //    float: left;
+
+// document.querySelector("weird-info").addEventListener("mouseover", function () {
+//   const weirdinfo = document.querySelector("weird-info");
+//   const weirdpicture = document.getElementById("weirdpicture");
+//   weirdpicture.style.display = none;
+// });
