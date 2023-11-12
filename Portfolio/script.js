34fa9962f7c49987df4d02fccae0592eb648bcb3
Joseph Maddox <Lavertsss@gmail.com>
2023-11-12 12:24:46 -0500

    Animation 50% done

---
+++ Portfolio/script.js
@@ -0,0 +1,23 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("blob"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "data.json",
+});
+
+// function translatefunc() {
+//   // Code for Chrome, Safari, Opera
+//   document.getElementsByClassName("body").style.WebkitTransform =
+//     "translate(20px)";
+//   // Code for IE9
+//   document.getElementsByClassName("body").style.msTransform = "translate(20px)";
+//   // Standard syntax
+//   document.getElementsByClassName("body").style.transform = "translate(20px)";
+// }
+
+// document
+//   .getElementById("page-container")
+//   .addEventListener("onmouseover", function () {
+//     translatefunc();
+//   });
